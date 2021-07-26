a879c1024d35e063be05526d7cc48ff9bceea743
Le Simoes <lesimoes0@gmail.com>
2021-07-26 18:00:41 -0300

    adicinado selecionar um item por categoria

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,32 @@
 function selecionarPrato (elemento) {
-    console.log(elemento)
+    
+    const selecionado = document.querySelector(".prato .selecionado");
+
+    if (selecionado !== null) {
+        selecionado.classList.remove("selecionado");
+    }
+    
+    elemento.classList.add("selecionado");
+}
+
+function selecionarBebida (elemento) {
+    
+    const selecionado = document.querySelector(".bebida .selecionado");
+
+    if (selecionado !== null) {
+        selecionado.classList.remove("selecionado");
+    }
+    
+    elemento.classList.add("selecionado");
+}
+
+function selecionarSobremesa (elemento) {
+    
+    const selecionado = document.querySelector(".sobremesa .selecionado");
+
+    if (selecionado !== null) {
+        selecionado.classList.remove("selecionado");
+    }
+    
     elemento.classList.add("selecionado");
 }
